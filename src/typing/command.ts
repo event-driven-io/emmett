17c1c3dee8eea21054d60fd17fa430a722604649
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-02-11 12:15:42 +0100

    Added default command metadata with date now

--- src/typing/command.ts
+++ src/typing/command.ts
@@ -1,9 +1,11 @@
 import type { Flavour } from './';
 
+export type DefaultCommandMetadata = { now: Date };
+
 export type Command<
   CommandType extends string = string,
   CommandData extends Record<string, unknown> = Record<string, unknown>,
-  CommandMetaData extends Record<string, unknown> = Record<string, unknown>,
+  CommandMetaData extends Record<string, unknown> = DefaultCommandMetadata,
 > = Flavour<
   Readonly<{
     type: CommandType;
