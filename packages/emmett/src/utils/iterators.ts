db28181c76d25607d49476bee9d1eecf786d5d38
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-02-12 23:24:14 +0100

    Configured build pipeline to build workspaces

--- packages/emmett/src/utils/iterators.ts
+++ packages/emmett/src/utils/iterators.ts
@@ -1,10 +1,11 @@
 export const sum = (
   iterator: Iterator<number, number, number> | Iterator<number>,
 ) => {
-  let value: number,
-    done,
+  let value,
+    done: boolean | undefined,
     sum = 0;
   do {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     ({ value, done } = iterator.next());
     sum += value || 0;
   } while (!done);
