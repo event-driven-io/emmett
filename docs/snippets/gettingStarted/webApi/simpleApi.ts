8497fd99c0a89e30d8382c8a8ddc6cde16c46854
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-02-21 20:59:02 +0100

    Dropped eslint ignores

--- docs/snippets/gettingStarted/webApi/simpleApi.ts
+++ docs/snippets/gettingStarted/webApi/simpleApi.ts
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import {
   assertNotEmptyString,
   assertPositiveNumber,
