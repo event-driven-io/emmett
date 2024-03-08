7f2164f521560b426bde131d743c23f794ba4d45
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-03-08 09:50:07 +0100

    Added test project to ensure compatibility with ESM
    Added also initial CI script to ensure that new version will keep compatibility

---
+++ e2e/esmCompatibility/src/index.ts
@@ -0,0 +1,23 @@
+import { CommandHandler, getInMemoryEventStore } from '@event-driven-io/emmett';
+import { randomUUID } from 'crypto';
+import {
+  AddProductItemToShoppingCart,
+  addProductItem,
+} from './shoppingCart/businessLogic';
+import { evolve, getInitialState } from './shoppingCart/shoppingCart';
+
+export const handle = CommandHandler(evolve, getInitialState);
+const store = getInMemoryEventStore();
+
+const shoppingCartId = '123';
+const command: AddProductItemToShoppingCart = {
+  type: 'AddProductItemToShoppingCart',
+  data: {
+    productItem: { productId: randomUUID(), quantity: 10, unitPrice: 10 },
+    shoppingCartId,
+  },
+};
+const x = await handle(store, shoppingCartId, (state) =>
+  addProductItem(command, state),
+);
+console.log(x);
