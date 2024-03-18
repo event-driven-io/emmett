79b5582a16f2fd6d65883c4515b423ad8cec9f93
Thiago Martins <thiago_valentim1@hotmail.com>
2024-03-18 16:39:59 -0300

    refactor(emmet, emmet-esdb): refactor utility function to work with async hooks

--- src/packages/emmett-esdb/src/eventStore/eventstoreDBEventStore.e2e.spec.ts
+++ src/packages/emmett-esdb/src/eventStore/eventstoreDBEventStore.e2e.spec.ts
@@ -1,94 +1,29 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import {
-  evolve,
-  getInitialState,
-  type EventStore,
-  type PricedProductItem,
-  type ShoppingCartEvent,
+  testAggregateStream,
+  type EventStoreFactory,
 } from '@event-driven-io/emmett';
 import {
   EventStoreDBContainer,
   StartedEventStoreDBContainer,
 } from '@event-driven-io/emmett-testcontainers';
-import assert from 'node:assert';
-import { randomUUID } from 'node:crypto';
-import { after, before, describe, it } from 'node:test';
+import { describe } from 'node:test';
 import { getEventStoreDBEventStore } from './eventstoreDBEventStore';
 
-describe('EventStoreDBEventStore', () => {
+describe('EventStoreDBEventStore', async () => {
   let esdbContainer: StartedEventStoreDBContainer;
-  let eventStore: EventStore;
 
-  before(async () => {
+  const eventStoreFactory: EventStoreFactory = async () => {
     esdbContainer = await new EventStoreDBContainer().start();
-    eventStore = getEventStoreDBEventStore(esdbContainer.getClient());
-  });
+    return getEventStoreDBEventStore(esdbContainer.getClient());
+  };
 
-  after(() => {
-    return esdbContainer.stop();
-  });
+  const teardownHook = async () => {
+    await esdbContainer.stop();
+  };
 
-  describe('aggregateStream', () => {
-    it('When called with `to` allows time traveling', async () => {
-      // Given
-      const productItem: PricedProductItem = {
-        productId: '123',
-        quantity: 10,
-        price: 3,
-      };
-      const discount = 10;
-      const shoppingCartId = randomUUID();
-
-      await eventStore.appendToStream<ShoppingCartEvent>(shoppingCartId, [
-        { type: 'ProductItemAdded', data: { productItem } },
-      ]);
-      await eventStore.appendToStream<ShoppingCartEvent>(shoppingCartId, [
-        { type: 'ProductItemAdded', data: { productItem } },
-      ]);
-      await eventStore.appendToStream<ShoppingCartEvent>(shoppingCartId, [
-        { type: 'DiscountApplied', data: { percent: discount } },
-      ]);
-
-      // when
-      const resultAt1 = await eventStore.aggregateStream(shoppingCartId, {
-        evolve,
-        getInitialState,
-        read: { to: 1n },
-      });
-      const resultAt2 = await eventStore.aggregateStream(shoppingCartId, {
-        evolve,
-        getInitialState,
-        read: { to: 2n },
-      });
-      const resultAt3 = await eventStore.aggregateStream(shoppingCartId, {
-        evolve,
-        getInitialState,
-        read: { to: 3n },
-      });
-
-      // then
-      assert.ok(resultAt1);
-      assert.ok(resultAt2);
-      assert.ok(resultAt3);
-
-      // Note that ESDB counts from 0
-      assert.equal(resultAt1.currentStreamVersion, 0);
-      assert.deepEqual(resultAt1.state, {
-        productItems: [productItem],
-        totalAmount: 30,
-      });
-
-      assert.equal(resultAt2.currentStreamVersion, 1);
-      assert.deepEqual(resultAt2.state, {
-        productItems: [productItem, productItem],
-        totalAmount: 60,
-      });
-
-      assert.equal(resultAt3.currentStreamVersion, 2);
-      assert.deepEqual(resultAt3.state, {
-        productItems: [productItem, productItem],
-        totalAmount: 54,
-      });
-    });
+  await testAggregateStream(eventStoreFactory, {
+    teardownHook,
+    getInitialIndex: () => 0n,
   });
 });
