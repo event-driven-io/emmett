cefd3793544642758cbb6b7ea8d1e2e233306b1c
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-04-28 14:29:43 +0200

    Added default handling for Mac M1 ARM64 image for EventStoreDB TestContainer.
    Changed EventStoreDB test container options to be in the nested object and false by default. That should make them more straightforward.

--- src/packages/emmett-testcontainers/src/eventStore/eventStoreDBContainer.ts
+++ src/packages/emmett-testcontainers/src/eventStore/eventStoreDBContainer.ts
@@ -6,36 +6,54 @@ import {
 } from 'testcontainers';
 import type { Environment } from 'testcontainers/build/types';
 
-const EVENTSTOREDB_PORT = 2113;
-const EVENTSTOREDB_TCP_PORT = 1113;
-const EVENTSTOREDB_TCP_PORTS = [EVENTSTOREDB_TCP_PORT, EVENTSTOREDB_PORT];
-const EVENTSTOREDB_IMAGE_NAME = 'eventstore/eventstore';
-const EVENTSTOREDB_IMAGE_TAG = '23.10.1-bookworm-slim';
+export const EVENTSTOREDB_PORT = 2113;
+export const EVENTSTOREDB_TCP_PORT = 1113;
+export const EVENTSTOREDB_TCP_PORTS = [
+  EVENTSTOREDB_TCP_PORT,
+  EVENTSTOREDB_PORT,
+];
+export const EVENTSTOREDB_IMAGE_NAME = 'eventstore/eventstore';
+export const EVENTSTOREDB_IMAGE_TAG = '23.10.1-bookworm-slim';
+export const EVENTSTOREDB_ARM64_IMAGE_TAG = '23.10.1-alpha-arm64v8';
+
+export const EVENTSTOREDB_DEFAULT_IMAGE = `${EVENTSTOREDB_IMAGE_NAME}:${process.arch !== 'arm64' ? EVENTSTOREDB_IMAGE_TAG : EVENTSTOREDB_ARM64_IMAGE_TAG}`;
+
+export type EventStoreDBContainerOptions = {
+  disableProjections?: boolean;
+  isSecure?: boolean;
+  useFileStorage?: boolean;
+  withoutReuse?: boolean;
+};
+
+export const defaultEventStoreDBContainerOptions: EventStoreDBContainerOptions =
+  {
+    disableProjections: false,
+    isSecure: false,
+    useFileStorage: false,
+    withoutReuse: false,
+  };
 
 export class EventStoreDBContainer extends GenericContainer {
   private readonly tcpPorts = EVENTSTOREDB_TCP_PORTS;
 
   constructor(
-    image = `${EVENTSTOREDB_IMAGE_NAME}:${EVENTSTOREDB_IMAGE_TAG}`,
-    runProjections = true,
-    isInsecure = true,
-    emptyDatabase = true,
-    withoutReuse = false,
+    image = EVENTSTOREDB_DEFAULT_IMAGE,
+    options: EventStoreDBContainerOptions = defaultEventStoreDBContainerOptions,
   ) {
     super(image);
 
     const environment: Environment = {
-      ...(runProjections
+      ...(!options.disableProjections
         ? {
             EVENTSTORE_RUN_PROJECTIONS: 'ALL',
           }
         : {}),
-      ...(isInsecure
+      ...(!options.isSecure
         ? {
             EVENTSTORE_INSECURE: 'true',
           }
         : {}),
-      ...(!emptyDatabase
+      ...(options.useFileStorage
         ? {
             EVENTSTORE_MEM_DB: 'false',
             EVENTSTORE_DB: '/data/integration-tests',
@@ -51,7 +69,7 @@ export class EventStoreDBContainer extends GenericContainer {
 
     this.withEnvironment(environment).withExposedPorts(...this.tcpPorts);
 
-    if (!withoutReuse) this.withReuse();
+    if (!options.withoutReuse) this.withReuse();
   }
 
   async start(): Promise<StartedEventStoreDBContainer> {
