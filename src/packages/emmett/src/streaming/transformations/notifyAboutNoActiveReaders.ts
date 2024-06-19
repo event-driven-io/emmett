88356ed34254fafbbc25a47c37d6580ff30c95ed
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-06-19 14:52:27 +0200

    Added tests for notifying about no active readers

---
+++ src/packages/emmett/src/streaming/transformations/notifyAboutNoActiveReaders.ts
@@ -0,0 +1,61 @@
+import { v4 as uuid } from 'uuid';
+import { TransformStream } from 'web-streams-polyfill';
+
+export const notifyAboutNoActiveReadersStream = <Item>(
+  onNoActiveReaderCallback: (
+    stream: NotifyAboutNoActiveReadersStream<Item>,
+  ) => void,
+  options: { streamId?: string; intervalCheckInMs?: number } = {},
+) => new NotifyAboutNoActiveReadersStream(onNoActiveReaderCallback, options);
+
+export class NotifyAboutNoActiveReadersStream<Item> extends TransformStream<
+  Item,
+  Item
+> {
+  private checkInterval: NodeJS.Timeout | null = null;
+  public readonly streamId: string;
+  private _isStopped: boolean = false;
+  public get hasActiveSubscribers() {
+    return !this._isStopped;
+  }
+
+  constructor(
+    private onNoActiveReaderCallback: (
+      stream: NotifyAboutNoActiveReadersStream<Item>,
+    ) => void,
+    options: { streamId?: string; intervalCheckInMs?: number } = {},
+  ) {
+    super({
+      cancel: (reason) => {
+        console.log('Stream was canceled. Reason:', reason);
+        this.stopChecking();
+      },
+    });
+    this.streamId = options?.streamId ?? uuid();
+
+    this.onNoActiveReaderCallback = onNoActiveReaderCallback;
+
+    this.startChecking(options?.intervalCheckInMs ?? 20);
+  }
+
+  private startChecking(interval: number) {
+    this.checkInterval = setInterval(() => {
+      this.checkNoActiveReader();
+    }, interval);
+  }
+
+  private stopChecking() {
+    if (!this.checkInterval) return;
+
+    this._isStopped = true;
+    clearInterval(this.checkInterval);
+    this.checkInterval = null;
+    this.onNoActiveReaderCallback(this);
+  }
+
+  private checkNoActiveReader() {
+    if (!this.readable.locked && !this._isStopped) {
+      this.stopChecking();
+    }
+  }
+}
---
+++ src/packages/emmett/src/streaming/transformations/notifyAboutNoActiveReaders.unit.spec.ts
@@ -0,0 +1,135 @@
+import { describe, it } from 'node:test';
+import { ReadableStream, TransformStream } from 'web-streams-polyfill';
+import { assertEqual, assertFalse, assertOk, assertTrue } from '../../testing';
+import { collectStream } from '../collect';
+import {
+  NotifyAboutNoActiveReadersStream,
+  notifyAboutNoActiveReadersStream,
+} from './notifyAboutNoActiveReaders';
+
+void describe('NotifyAboutNoActiveReadersStream', () => {
+  const customStreamId = 'custom-id';
+
+  void it('should initialize with default options', () => {
+    const mockCallback = () => {};
+    const stream = notifyAboutNoActiveReadersStream(mockCallback);
+
+    assertOk(stream.streamId);
+  });
+
+  void it('should initialize with custom options', () => {
+    const mockCallback = () => {};
+    const intervalCheckInMs = 50;
+
+    const stream = notifyAboutNoActiveReadersStream(mockCallback, {
+      streamId: customStreamId,
+      intervalCheckInMs,
+    });
+
+    assertEqual(stream.streamId, customStreamId);
+  });
+
+  void it('should start and stop checking intervals correctly', async () => {
+    let callbackInvoked = false;
+
+    const mockCallback = (stream: NotifyAboutNoActiveReadersStream<number>) => {
+      callbackInvoked = true;
+      assertEqual(stream.streamId, customStreamId);
+    };
+
+    const stream = notifyAboutNoActiveReadersStream(mockCallback, {
+      intervalCheckInMs: 10,
+      streamId: customStreamId,
+    });
+
+    const reader = stream.readable.getReader();
+    reader.releaseLock();
+
+    await new Promise((resolve) => setTimeout(resolve, 20));
+
+    assertTrue(callbackInvoked);
+    assertFalse(stream.hasActiveSubscribers);
+  });
+
+  void it('triggers callback after the piped stream is fully read and closed', async () => {
+    let callbackInvoked = false;
+    const chunksCount = 5;
+
+    const mockCallback = (stream: NotifyAboutNoActiveReadersStream<number>) => {
+      callbackInvoked = true;
+      assertEqual(stream.streamId, customStreamId);
+    };
+
+    // Create a source stream that generates some data
+    const sourceStream = new ReadableStream({
+      start(controller) {
+        for (let i = 0; i < chunksCount; i++) {
+          controller.enqueue(i);
+        }
+        controller.close(); // Close the stream after enqueuing all data
+      },
+    });
+
+    const stream = notifyAboutNoActiveReadersStream(mockCallback, {
+      intervalCheckInMs: 10,
+      streamId: customStreamId,
+    });
+
+    // Pipe the source stream into the NotifyAboutNoActiveReadersStream
+    const pipedStream = sourceStream.pipeThrough(stream);
+
+    // Read all data from the pipedStream to simulate full consumption
+    const chunks = await collectStream(pipedStream);
+    assertEqual(chunks.length, 5);
+
+    await new Promise((resolve) => setTimeout(resolve, 20));
+
+    assertTrue(callbackInvoked);
+    assertFalse(stream.hasActiveSubscribers);
+  });
+
+  void it('detects no active readers when piped from another active stream', async () => {
+    let callbackInvoked = false;
+    const chunksCount = 5;
+
+    const mockCallback = (stream: NotifyAboutNoActiveReadersStream<number>) => {
+      callbackInvoked = true;
+      assertEqual(stream.streamId, customStreamId);
+    };
+
+    // Create a source stream that generates some data
+    const sourceStream = new ReadableStream({
+      start(controller) {
+        for (let i = 0; i < chunksCount; i++) {
+          controller.enqueue(i);
+        }
+        controller.close(); // Close the stream after enqueuing all data
+      },
+    });
+
+    const anotherTransform = new TransformStream({
+      transform(chunk, controller) {
+        controller.enqueue(chunk);
+      },
+    });
+
+    const stream = notifyAboutNoActiveReadersStream(mockCallback, {
+      intervalCheckInMs: 10,
+      streamId: customStreamId,
+    });
+
+    // Pipe the source stream into the NotifyAboutNoActiveReadersStream
+    const pipedStream = sourceStream
+      .pipeThrough(stream)
+      .pipeThrough(anotherTransform);
+
+    // Read all data from the pipedStream to simulate full consumption
+    const chunks = await collectStream(pipedStream);
+    assertEqual(chunks.length, 5);
+
+    await new Promise((resolve) => setTimeout(resolve, 20));
+
+    assertTrue(callbackInvoked);
+    assertFalse(stream.hasActiveSubscribers);
+  });
+});
