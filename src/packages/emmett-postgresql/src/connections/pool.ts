eecd789013477517273fdc74c876d6e8069de292
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-07-10 10:12:24 +0200

    Moved Postgres related code based on Pongo implementation

---
+++ src/packages/emmett-postgresql/src/connections/pool.ts
@@ -0,0 +1,36 @@
+import pg from 'pg';
+
+const pools: Map<string, pg.Pool> = new Map();
+
+export const getPool = (
+  connectionStringOrOptions: string | pg.PoolConfig,
+): pg.Pool => {
+  const connectionString =
+    typeof connectionStringOrOptions === 'string'
+      ? connectionStringOrOptions
+      : connectionStringOrOptions.connectionString!;
+
+  const poolOptions =
+    typeof connectionStringOrOptions === 'string'
+      ? { connectionString }
+      : connectionStringOrOptions;
+
+  //TODO: this should include database name resolution for key
+  return (
+    pools.get(connectionString) ??
+    pools.set(connectionString, new pg.Pool(poolOptions)).get(connectionString)!
+  );
+};
+
+export const endPool = async (connectionString: string): Promise<void> => {
+  const pool = pools.get(connectionString);
+  if (pool) {
+    await pool.end();
+    pools.delete(connectionString);
+  }
+};
+
+export const endAllPools = () =>
+  Promise.all(
+    [...pools.keys()].map((connectionString) => endPool(connectionString)),
+  );
--- src/packages/emmett-postgresql/src/index.ts
+++ src/packages/emmett-postgresql/src/index.ts
@@ -1,1 +1,4 @@
+export * from './connections';
 export * from './eventStore';
+export * from './execute';
+export * from './sql';
