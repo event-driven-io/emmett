6be99e038c55b53970bcd781bb9d183879b6c3f8
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-08-07 14:42:17 +0200

    Added raw sql projections to support raw SQL handling
    Generalised also projection definition

--- src/packages/emmett/src/index.ts
+++ src/packages/emmett/src/index.ts
@@ -2,6 +2,7 @@ export * from './commandHandling';
 export * from './errors';
 export * from './eventStore';
 export * from './messageBus';
+export * from './projections';
 export * from './serialization';
 export * from './streaming';
 export * from './subscriptions';
