2b76534ca2e8cf63821e2e3e06baa6087df03164
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-08-25 13:07:19 +0200

    Added client shopping cart summary projection

--- samples/webApi/expressjs-with-postgresql/src/shoppingCarts/api.ts
+++ samples/webApi/expressjs-with-postgresql/src/shoppingCarts/api.ts
@@ -189,6 +189,22 @@ export const shoppingCartApi =
         });
       }),
     );
+
+    // Get Shopping Cart
+    router.get(
+      '/clients/:clientId/shopping-carts/summary',
+      on(async (request: GetShoppingCartRequest) => {
+        const clientId = assertNotEmptyString(request.params.clientId);
+
+        const result = await getClientShoppingSummary(readStore, clientId);
+
+        if (result === null) return NotFound();
+
+        return OK({
+          body: result,
+        });
+      }),
+    );
   };
 
 // Add Product Item
