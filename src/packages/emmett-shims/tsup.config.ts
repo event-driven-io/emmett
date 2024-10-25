1c781ff3d1c303485d1b716157a2d1c01e1413c5
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-10-25 15:16:19 +0200

    Used useDefineForClassFields setting to have a proper class variable initialisation for commonjs

--- src/packages/emmett-shims/tsup.config.ts
+++ src/packages/emmett-shims/tsup.config.ts
@@ -4,27 +4,12 @@ const env = process.env.NODE_ENV;
 
 export default defineConfig([
   {
-    format: ['cjs'],
+    format: ['esm', 'cjs'],
     splitting: true,
     clean: true,
     dts: true,
     // TODO: For some reason minified code doesn't work for cjs
     minify: false, //env === 'production',
-    bundle: true, //env === 'production',
-    skipNodeModulesBundle: true,
-    watch: env === 'development',
-    target: 'esnext',
-    outDir: 'dist', //env === 'production' ? 'dist' : 'lib',
-    entry: ['src/index.ts'],
-    sourcemap: true,
-    tsconfig: 'tsconfig.build.json', // workaround for https://github.com/egoist/tsup/issues/571#issuecomment-1760052931
-  },
-  {
-    format: ['esm'],
-    splitting: true,
-    clean: true,
-    dts: true,
-    minify: true, //env === 'production',
     bundle: true, //env === 'production',
     skipNodeModulesBundle: true,
     watch: env === 'development',
