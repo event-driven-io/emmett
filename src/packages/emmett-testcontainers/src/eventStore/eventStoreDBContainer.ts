b5ad1ec329e0e2c025ab5f3af9c250c1ab1385a3
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-12 10:34:33 +0100

    Used shared EventStoreDB container in tests
    Made it also reusable and waiting for healthcheck

--- src/packages/emmett-testcontainers/src/eventStore/eventStoreDBContainer.ts
+++ src/packages/emmett-testcontainers/src/eventStore/eventStoreDBContainer.ts
@@ -2,14 +2,15 @@ import { EventStoreDBClient } from '@eventstore/db-client';
 import {
   AbstractStartedContainer,
   GenericContainer,
+  Wait,
   type StartedTestContainer,
 } from 'testcontainers';
 import type { Environment } from 'testcontainers/build/types';
 
 export const EVENTSTOREDB_PORT = 2113;
 export const EVENTSTOREDB_IMAGE_NAME = 'eventstore/eventstore';
-export const EVENTSTOREDB_IMAGE_TAG = '24.10.0-bookworm-slim';
-export const EVENTSTOREDB_ARM64_IMAGE_TAG = '24.10.0-alpha-arm64v8';
+export const EVENTSTOREDB_IMAGE_TAG = '23.10.1-bookworm-slim';
+export const EVENTSTOREDB_ARM64_IMAGE_TAG = '23.10.1-alpha-arm64v8';
 
 export const EVENTSTOREDB_DEFAULT_IMAGE = `${EVENTSTOREDB_IMAGE_NAME}:${process.arch !== 'arm64' ? EVENTSTOREDB_IMAGE_TAG : EVENTSTOREDB_ARM64_IMAGE_TAG}`;
 
@@ -61,6 +62,10 @@ export class EventStoreDBContainer extends GenericContainer {
     this.withEnvironment(environment).withExposedPorts(EVENTSTOREDB_PORT);
 
     if (options.withReuse) this.withReuse();
+
+    this.withWaitStrategy(
+      Wait.forAll([Wait.forHealthCheck(), Wait.forListeningPorts()]),
+    );
   }
 
   async start(): Promise<StartedEventStoreDBContainer> {
@@ -85,29 +90,40 @@ let container: EventStoreDBContainer | null = null;
 let startedContainer: StartedEventStoreDBContainer | null = null;
 let startedCount = 0;
 
-export const getEventStoreDBTestContainer = async () => {
+export const getSharedEventStoreDBTestContainer = async () => {
   if (startedContainer) return startedContainer;
 
   if (!container)
-    container = new EventStoreDBContainer(EVENTSTOREDB_DEFAULT_IMAGE);
+    container = new EventStoreDBContainer(EVENTSTOREDB_DEFAULT_IMAGE, {
+      withReuse: true,
+    });
 
   startedContainer = await container.start();
   startedCount++;
+
+  container.withLogConsumer((stream) =>
+    stream
+      .on('data', (line) => console.log(line))
+      .on('err', (line) => console.error(line))
+      .on('end', () => console.log('Stream closed')),
+  );
 
   return startedContainer;
 };
 
 export const getSharedTestEventStoreDBClient = async () => {
-  return (await getEventStoreDBTestContainer()).getClient();
+  return (await getSharedEventStoreDBTestContainer()).getClient();
 };
 
-export const releaseShartedEventStoreDBTestContainer = async () => {
-  if (startedContainer && --startedCount === 0)
+export const releaseSharedEventStoreDBTestContainer = async () => {
+  const containerToStop = startedContainer;
+  if (containerToStop && --startedCount === 0) {
     try {
-      await startedContainer.stop();
+      startedContainer = null;
+      container = null;
+      await containerToStop.stop();
     } catch {
       /* do nothing */
     }
-  container = null;
-  startedContainer = null;
+  }
 };
