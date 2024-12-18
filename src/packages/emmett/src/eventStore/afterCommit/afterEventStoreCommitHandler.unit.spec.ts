a0d0580e739746118c7e17b8d6f6ecb6bfaa4422
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-12-18 15:33:07 +0100

    Added a dedicated set of tests for publishing messages after commit helper

---
+++ src/packages/emmett/src/eventStore/afterCommit/afterEventStoreCommitHandler.unit.spec.ts
@@ -0,0 +1,187 @@
+import { describe, it } from 'node:test';
+import { v7 as uuid } from 'uuid';
+import { assertDeepEqual, assertEqual } from '../../testing';
+import type {
+  Event,
+  ReadEvent,
+  ReadEventMetadataWithGlobalPosition,
+} from '../../typing';
+import type { EventStore } from '../eventStore';
+import { type InMemoryReadEvent } from '../inMemoryEventStore';
+import { tryPublishMessagesAfterCommit } from './afterEventStoreCommitHandler';
+
+type TestEvent = Event<'test', { counter: number }, { some: boolean }>;
+
+type TestEventStore = EventStore<ReadEventMetadataWithGlobalPosition>;
+
+void describe('InMemoryEventStore onAfterCommit', () => {
+  void it('calls onAfterCommit hook after events append', async () => {
+    // Given
+    const appendedEvents: ReadEvent<
+      Event,
+      ReadEventMetadataWithGlobalPosition
+    >[] = [];
+
+    const streamName = `test:${uuid()}`;
+    let counter = 0;
+    const events: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[] = [
+      {
+        type: 'test',
+        data: { counter: ++counter },
+        metadata: {
+          some: true,
+          eventId: uuid(),
+          globalPosition: 1n,
+          streamName,
+          streamPosition: 1n,
+        },
+      },
+      {
+        type: 'test',
+        data: { counter: ++counter },
+        metadata: {
+          some: false,
+          eventId: uuid(),
+          globalPosition: 1n,
+          streamName,
+          streamPosition: 1n,
+        },
+      },
+    ];
+
+    // When
+    await tryPublishMessagesAfterCommit<TestEventStore>(events, {
+      onAfterCommit: (
+        events: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[],
+      ) => {
+        appendedEvents.push(...events);
+      },
+    });
+
+    // Then
+    assertEqual(2, appendedEvents.length);
+    assertDeepEqual(appendedEvents, events);
+  });
+
+  void it('calls onAfterCommit hook exactly once for each events append', async () => {
+    // Given
+    const appendedEvents: InMemoryReadEvent[] = [];
+
+    const streamName = `test:${uuid()}`;
+    let counter = 0;
+    const events: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[] = [
+      {
+        type: 'test',
+        data: { counter: ++counter },
+        metadata: {
+          some: true,
+          eventId: uuid(),
+          globalPosition: 1n,
+          streamName,
+          streamPosition: 1n,
+        },
+      },
+      {
+        type: 'test',
+        data: { counter: ++counter },
+        metadata: {
+          some: false,
+          eventId: uuid(),
+          globalPosition: 1n,
+          streamName,
+          streamPosition: 1n,
+        },
+      },
+    ];
+    const nextEvents: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[] =
+      [
+        {
+          type: 'test',
+          data: { counter: ++counter },
+          metadata: {
+            some: true,
+            eventId: uuid(),
+            globalPosition: 1n,
+            streamName,
+            streamPosition: 1n,
+          },
+        },
+        {
+          type: 'test',
+          data: { counter: ++counter },
+          metadata: {
+            some: false,
+            eventId: uuid(),
+            globalPosition: 1n,
+            streamName,
+            streamPosition: 1n,
+          },
+        },
+      ];
+
+    // When
+    await tryPublishMessagesAfterCommit<TestEventStore>(events, {
+      onAfterCommit: (
+        events: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[],
+      ) => {
+        appendedEvents.push(...events);
+      },
+    });
+    await tryPublishMessagesAfterCommit<TestEventStore>(nextEvents, {
+      onAfterCommit: (
+        events: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[],
+      ) => {
+        appendedEvents.push(...events);
+      },
+    });
+
+    // Then
+    assertEqual(4, appendedEvents.length);
+    assertDeepEqual(appendedEvents, [...events, ...nextEvents]);
+  });
+
+  void it('silently fails when onAfterCommit hook failed but still keeps events', async () => {
+    // Given
+    const appendedEvents: InMemoryReadEvent[] = [];
+
+    const streamName = `test:${uuid()}`;
+    let counter = 0;
+    const events: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[] = [
+      {
+        type: 'test',
+        data: { counter: ++counter },
+        metadata: {
+          some: true,
+          eventId: uuid(),
+          globalPosition: 1n,
+          streamName,
+          streamPosition: 1n,
+        },
+      },
+      {
+        type: 'test',
+        data: { counter: ++counter },
+        metadata: {
+          some: false,
+          eventId: uuid(),
+          globalPosition: 1n,
+          streamName,
+          streamPosition: 1n,
+        },
+      },
+    ];
+
+    // When
+    await tryPublishMessagesAfterCommit<TestEventStore>(events, {
+      onAfterCommit: (
+        events: ReadEvent<Event, ReadEventMetadataWithGlobalPosition>[],
+      ) => {
+        appendedEvents.push(...events);
+      },
+    });
+
+    // Then
+    assertEqual(2, appendedEvents.length);
+    assertDeepEqual(appendedEvents, events);
+  });
+});
