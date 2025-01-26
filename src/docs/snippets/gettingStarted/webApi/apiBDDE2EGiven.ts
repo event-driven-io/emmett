f3e926b06d10281317002b9c906994d638a20277
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-01-26 09:53:44 +0100

    Used PostgreSQL storage in documentation instead of EventStoreDB, as it's the most popular choice and the most feature rich option

--- src/docs/snippets/gettingStarted/webApi/apiBDDE2EGiven.ts
+++ src/docs/snippets/gettingStarted/webApi/apiBDDE2EGiven.ts
@@ -7,34 +7,34 @@ const now = new Date();
 
 // #region test-container
 import {
-  EventStoreDBContainer,
-  type StartedEventStoreDBContainer,
-} from '@event-driven-io/emmett-testcontainers';
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from '@testcontainers/postgresql';
 
-let esdbContainer: StartedEventStoreDBContainer;
+let postgreSQLContainer: StartedPostgreSqlContainer;
 void describe('ShoppingCart E2E', () => {
   // Set up a container before all tests
   before(async () => {
-    esdbContainer = await new EventStoreDBContainer().start();
+    postgreSQLContainer = await new PostgreSqlContainer().start();
   });
 
   // Stop container once we finished testing
   after(() => {
-    return esdbContainer.stop();
+    return postgreSQLContainer.stop();
   });
   // (...) Tests will go here
 });
 // #endregion test-container
 
 // #region given
-import { getEventStoreDBEventStore } from '@event-driven-io/emmett-esdb';
 import {
   ApiE2ESpecification,
   getApplication,
 } from '@event-driven-io/emmett-expressjs';
+import { getPostgreSQLEventStore } from '@event-driven-io/emmett-postgresql';
 
 const given = ApiE2ESpecification.for(
-  () => getEventStoreDBEventStore(esdbContainer.getClient()),
+  () => getPostgreSQLEventStore(postgreSQLContainer.getConnectionUri()),
   (eventStore) =>
     getApplication({
       apis: [
