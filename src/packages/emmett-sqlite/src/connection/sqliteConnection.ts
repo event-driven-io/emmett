5dee6a142dd57f4145a833da9db381bf42312560
Lucca Hellriegel <lucca.hellriegel@gmx.net>
2025-03-02 23:13:57 +0100

    sqlite: extend SQLiteConnection

--- src/packages/emmett-sqlite/src/connection/sqliteConnection.ts
+++ src/packages/emmett-sqlite/src/connection/sqliteConnection.ts
@@ -4,9 +4,10 @@ export type Parameters = object | string | bigint | number | boolean | null;
 
 export type SQLiteConnection = {
   close: () => void;
-  command: (sql: string, values?: Parameters[]) => Promise<void>;
+  command: (sql: string, values?: Parameters[]) => Promise<sqlite3.RunResult>;
   query: <T>(sql: string, values?: Parameters[]) => Promise<T[]>;
   querySingle: <T>(sql: string, values?: Parameters[]) => Promise<T | null>;
+  withTransaction: <T>(fn: () => Promise<T>) => Promise<T>;
 };
 
 export interface SQLiteError extends Error {
@@ -37,15 +38,19 @@ export const sqliteConnection = (
   return {
     close: (): void => db.close(),
     command: (sql: string, params?: Parameters[]) =>
-      new Promise((resolve, reject) => {
-        db.run(sql, params ?? [], (err: Error | null) => {
-          if (err) {
-            reject(err);
-            return;
-          }
+      new Promise<sqlite3.RunResult>((resolve, reject) => {
+        db.run(
+          sql,
+          params ?? [],
+          function (this: sqlite3.RunResult, err: Error | null) {
+            if (err) {
+              reject(err);
+              return;
+            }
 
-          resolve();
-        });
+            resolve(this);
+          },
+        );
       }),
     query: <T>(sql: string, params?: Parameters[]): Promise<T[]> =>
       new Promise((resolve, reject) => {
@@ -69,5 +74,50 @@ export const sqliteConnection = (
           resolve(result);
         });
       }),
+    withTransaction: <T>(fn: () => Promise<T>) =>
+      new Promise<T>((resolve, reject) => {
+        beginTransaction(db)
+          .then(() => fn())
+          .then((result) => commitTransaction(db).then(() => resolve(result)))
+          .catch((err: Error) =>
+            rollbackTransaction(db).then(() => reject(err)),
+          );
+      }),
   };
 };
+
+const beginTransaction = (db: sqlite3.Database) =>
+  new Promise<void>((resolve, reject) => {
+    db.run('BEGIN TRANSACTION', (err: Error | null) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
+
+const commitTransaction = (db: sqlite3.Database) =>
+  new Promise<void>((resolve, reject) => {
+    db.run('COMMIT', (err: Error | null) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
+
+const rollbackTransaction = (db: sqlite3.Database) =>
+  new Promise<void>((resolve, reject) => {
+    db.run('ROLLBACK', (err: Error | null) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
