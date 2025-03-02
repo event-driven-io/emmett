96eb86d88e667c6dd62c8c724ab2eebb1c78e024
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-03-02 14:10:35 +0100

    Added factory methods and typing for processors and checkpointing

--- src/packages/emmett/src/processors/processors.ts
+++ src/packages/emmett/src/processors/processors.ts
@@ -1,31 +1,51 @@
 import type { EmmettError } from '../errors';
 import type { ProjectionDefinition } from '../projections';
 import {
+  type AnyEvent,
   type AnyMessage,
   type AnyReadEventMetadata,
+  type AnyRecordedMessageMetadata,
+  type BatchRecordedMessageHandlerWithContext,
+  type CanHandle,
+  type DefaultRecord,
   type Event,
+  type GlobalPositionTypeOfRecordedMessageMetadata,
+  type Message,
   type MessageHandler,
   type MessageHandlerResult,
-  type ReadEvent,
-  type ReadEventMetadataWithGlobalPosition,
   type RecordedMessage,
+  type SingleMessageHandlerWithContext,
 } from '../typing';
 
-export type CurrentMessageProcessorPosition<
-  Position = { globalPosition: bigint },
-> = Position | 'BEGINNING' | 'END';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type CurrentMessageProcessorPosition<CheckpointType = any> =
+  | { lastCheckpoint: CheckpointType }
+  | 'BEGINNING'
+  | 'END';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type MessageProcessorStartFrom<CheckpointType = any> =
+  | CurrentMessageProcessorPosition<CheckpointType>
+  | 'CURRENT';
 
 export type MessageProcessor<
   MessageType extends AnyMessage = AnyMessage,
-  MessageMetaDataType extends AnyReadEventMetadata = AnyReadEventMetadata,
-  MessageProcessorStartOptions = unknown,
+  MessageMetadataType extends AnyReadEventMetadata = AnyReadEventMetadata,
+  HandlerContext extends DefaultRecord | undefined = undefined,
+  CheckpointType = GlobalPositionTypeOfRecordedMessageMetadata<MessageMetadataType>,
 > = {
   id: string;
   start: (
-    options: MessageProcessorStartOptions,
-  ) => Promise<CurrentMessageProcessorPosition | undefined>;
+    options: Partial<HandlerContext> & {
+      startFrom: MessageProcessorStartFrom<CheckpointType>;
+    },
+  ) => Promise<CurrentMessageProcessorPosition<CheckpointType> | undefined>;
   isActive: boolean;
-  handle: MessageHandler<MessageType, MessageMetaDataType>;
+  handle: BatchRecordedMessageHandlerWithContext<
+    MessageType,
+    MessageMetadataType,
+    Partial<HandlerContext>
+  >;
 };
 
 export const MessageProcessor = {
@@ -44,209 +64,345 @@ export const MessageProcessor = {
   },
 };
 
-export type MessageProcessorStartFrom =
-  | CurrentMessageProcessorPosition
-  | 'CURRENT';
+// export type MessageProcessingScope<HandlerContext = never> = (
+//   partialContext: Partial<HandlerContext>,
+// ) =>
+//   | ((
+//       handler: (context: HandlerContext) => MessageHandlerResult,
+//     ) => MessageHandlerResult)
+//   | ((
+//       handler: (context: HandlerContext) => Promise<MessageHandlerResult>,
+//     ) => Promise<MessageHandlerResult>);
 
-export type ProjectionProcessorOptions<EventType extends Event = Event> = {
-  processorId?: string;
-  version?: number;
-  projection: ProjectionDefinition<EventType>;
-  partition?: string;
-  startFrom?: MessageProcessorStartFrom;
-  stopAfter?: (
-    message: ReadEvent<EventType, ReadEventMetadataWithGlobalPosition>,
-  ) => boolean;
-};
+export type MessageProcessingScope<
+  HandlerContext extends DefaultRecord | undefined = undefined,
+> = (
+  partialContext: Partial<HandlerContext>,
+) => (
+  handler: (
+    context: HandlerContext,
+  ) => MessageHandlerResult | Promise<MessageHandlerResult>,
+) => MessageHandlerResult | Promise<MessageHandlerResult>;
 
 export type GenericMessageProcessorOptions<
   MessageType extends AnyMessage = AnyMessage,
-  MessageMetaDataType extends AnyReadEventMetadata = AnyReadEventMetadata,
+  MessageMetadataType extends AnyReadEventMetadata = AnyReadEventMetadata,
+  HandlerContext extends DefaultRecord = DefaultRecord,
+  CheckpointType = GlobalPositionTypeOfRecordedMessageMetadata<MessageMetadataType>,
 > = {
   processorId: string;
   version?: number;
   partition?: string;
-  startFrom?: MessageProcessorStartFrom;
+  startFrom?: MessageProcessorStartFrom<CheckpointType>;
   stopAfter?: (
-    message: RecordedMessage<MessageType, MessageMetaDataType>,
+    message: RecordedMessage<MessageType, MessageMetadataType>,
   ) => boolean;
+  processingScope?: MessageProcessingScope<HandlerContext>;
+  checkpoints?: {
+    read: ReadProcessorCheckpoint<CheckpointType, HandlerContext>;
+    store: StoreProcessorCheckpoint<
+      MessageType,
+      CheckpointType,
+      HandlerContext
+    >;
+  };
 } & (
   | {
-      eachMessage: MessageHandler<MessageType, MessageMetaDataType>;
+      eachMessage: SingleMessageHandlerWithContext<
+        MessageType,
+        MessageMetadataType,
+        HandlerContext
+      >;
+      canHandle?: CanHandle<MessageType>;
     }
-  | { eachBatch: MessageHandler<MessageType, MessageMetaDataType> }
+  | {
+      eachBatch: MessageHandler<
+        MessageType,
+        MessageMetadataType,
+        HandlerContext
+      >;
+      canHandle?: CanHandle<MessageType>;
+    }
 );
 
-// export type MessageProcessorOptions<EventType extends Event = Event> =
-//   | GenericMessageProcessorOptions<EventType>
-//   | ProjectionProcessorOptions<EventType>;
+export type ProjectionProcessorOptions<
+  EventType extends AnyEvent = AnyEvent,
+  MessageMetadataType extends AnyReadEventMetadata = AnyReadEventMetadata,
+  HandlerContext extends DefaultRecord = DefaultRecord,
+  CheckpointType = GlobalPositionTypeOfRecordedMessageMetadata<MessageMetadataType>,
+> = {
+  processorId?: string;
+  version?: number;
+  projection: ProjectionDefinition<
+    EventType,
+    MessageMetadataType,
+    HandlerContext
+  >;
+  partition?: string;
+  startFrom?: MessageProcessorStartFrom<CheckpointType>;
+  stopAfter?: (
+    message: RecordedMessage<EventType, MessageMetadataType>,
+  ) => boolean;
+  checkpoints?: {
+    read: ReadProcessorCheckpoint<CheckpointType, HandlerContext>;
+    store: StoreProcessorCheckpoint<EventType, CheckpointType, HandlerContext>;
+  };
+};
 
-// const genericMessageProcessor = <EventType extends Event = Event>(
-//   options: GenericMessageProcessorOptions<EventType>,
-// ): MessageProcessor => {
-//   const { eachMessage } = options;
-//   let isActive = true;
-//   //let lastProcessedPosition: bigint | null = null;
+export type MessageProcessorOptions<
+  MessageType extends AnyMessage = AnyMessage,
+  MessageMetadataType extends AnyReadEventMetadata = AnyReadEventMetadata,
+  HandlerContext extends DefaultRecord = DefaultRecord,
+  CheckpointType = GlobalPositionTypeOfRecordedMessageMetadata<MessageMetadataType>,
+> =
+  | GenericMessageProcessorOptions<
+      MessageType,
+      MessageMetadataType,
+      HandlerContext,
+      CheckpointType
+    >
+  | (MessageType extends Event
+      ? ProjectionProcessorOptions<
+          MessageType,
+          MessageMetadataType,
+          HandlerContext,
+          CheckpointType
+        >
+      : never);
 
-//   const poolOptions = {
-//     ...(options.connectionOptions ? options.connectionOptions : {}),
-//   };
-//   const processorConnectionString =
-//     'connectionString' in poolOptions ? poolOptions.connectionString : null;
+// export const defaultProcessingMessageProcessingScope = <HandlerContext = never>(partialContext: Partial<HandlerContext>) => ((context: HandlerContext) => (MessageHandlerResult | Promise<MessageHandlerResult>));
 
-//   const processorPool =
-//     'dumbo' in poolOptions
-//       ? (poolOptions.dumbo as NodePostgresPool)
-//       : processorConnectionString
-//         ? dumbo({
-//             connectionString: processorConnectionString,
-//             ...poolOptions,
-//           })
-//         : null;
+export type ReadProcessorCheckpointResult<CheckpointType = unknown> = {
+  lastCheckpoint: CheckpointType | null;
+};
 
-//   const getPool = (context: {
-//     pool?: Dumbo;
-//     connectionString?: string;
-//   }): { pool: Dumbo; connectionString: string } => {
-//     const connectionString =
-//       processorConnectionString ?? context.connectionString;
+export type ReadProcessorCheckpoint<
+  CheckpointType = unknown,
+  HandlerContext extends DefaultRecord = DefaultRecord,
+> = (
+  options: { processorId: string; partition?: string },
+  context: HandlerContext,
+) => Promise<ReadProcessorCheckpointResult<CheckpointType>>;
 
-//     if (!connectionString)
-//       throw new EmmettError(
-//         `PostgreSQL processor '${options.processorId}' is missing connection string. Ensure that you passed it through options`,
-//       );
+export type StoreProcessorCheckpointResult<CheckpointType = unknown> =
+  | {
+      success: true;
+      newCheckpoint: CheckpointType;
+    }
+  | { success: false; reason: 'IGNORED' | 'MISMATCH' };
 
-//     const pool =
-//       (!processorConnectionString ||
-//       connectionString == processorConnectionString
-//         ? context?.pool
-//         : processorPool) ?? processorPool;
+export type StoreProcessorCheckpoint<
+  MessageType extends Message = AnyMessage,
+  CheckpointType = unknown,
+  HandlerContext extends DefaultRecord | undefined = undefined,
+> =
+  | ((
+      options: {
+        message: MessageType;
+        processorId: string;
+        version: number | undefined;
+        lastCheckpoint: CheckpointType | null;
+        partition?: string;
+      },
+      context: HandlerContext,
+    ) => Promise<StoreProcessorCheckpointResult<CheckpointType | null>>)
+  | ((
+      options: {
+        message: MessageType;
+        processorId: string;
+        version: number | undefined;
+        lastCheckpoint: CheckpointType | null;
+        partition?: string;
+      },
+      context: HandlerContext,
+    ) => Promise<StoreProcessorCheckpointResult<CheckpointType>>);
 
-//     if (!pool)
-//       throw new EmmettError(
-//         `PostgreSQL processor '${options.processorId}' is missing connection string. Ensure that you passed it through options`,
-//       );
+const genericMessageProcessor = <
+  MessageType extends Message = AnyMessage,
+  MessageMetadataType extends AnyReadEventMetadata = AnyReadEventMetadata,
+  HandlerContext extends DefaultRecord = DefaultRecord,
+  CheckpointType = GlobalPositionTypeOfRecordedMessageMetadata<MessageMetadataType>,
+>(
+  options: GenericMessageProcessorOptions<
+    MessageType,
+    MessageMetadataType,
+    HandlerContext,
+    CheckpointType
+  >,
+): MessageProcessor<
+  MessageType,
+  MessageMetadataType,
+  HandlerContext,
+  CheckpointType
+> => {
+  const eachMessage: SingleMessageHandlerWithContext<
+    MessageType,
+    MessageMetadataType,
+    HandlerContext
+  > = 'eachMessage' in options ? options.eachMessage : () => Promise.resolve();
+  let isActive = true;
 
-//     return {
-//       connectionString,
-//       pool: pool,
-//     };
-//   };
+  const { checkpoints, processorId, partition } = options;
 
-//   return {
-//     id: options.processorId,
-//     start: async (
-//       execute: SQLExecutor,
-//     ): Promise<CurrentMessageProcessorPosition | undefined> => {
-//       isActive = true;
-//       if (options.startFrom !== 'CURRENT') return options.startFrom;
+  return {
+    id: options.processorId,
+    start: async (
+      startOptions: Partial<HandlerContext> & {
+        startFrom: MessageProcessorStartFrom<CheckpointType>;
+      },
+    ): Promise<CurrentMessageProcessorPosition<CheckpointType> | undefined> => {
+      isActive = true;
+      if (startOptions.startFrom !== 'CURRENT') return startOptions.startFrom;
 
-//       const { lastProcessedPosition } = await readProcessorCheckpoint(execute, {
-//         processorId: options.processorId,
-//         partition: options.partition,
-//       });
+      let lastCheckpoint: CheckpointType | null = null;
 
-//       if (lastProcessedPosition === null) return 'BEGINNING';
+      if (checkpoints) {
+        const readResult = await checkpoints?.read(
+          {
+            processorId: processorId,
+            partition: partition,
+          },
+          startOptions as HandlerContext & {
+            startFrom: MessageProcessorStartFrom<CheckpointType>;
+          },
+        );
+        lastCheckpoint = readResult.lastCheckpoint;
+      }
 
-//       return { globalPosition: lastProcessedPosition };
-//     },
-//     get isActive() {
-//       return isActive;
-//     },
-//     handle: async (
-//       { messages },
-//       context,
-//     ): Promise<MessageProcessorMessageHandlerResult> => {
-//       if (!isActive) return;
+      if (lastCheckpoint === null) return 'BEGINNING';
 
-//       const { pool, connectionString } = getPool(context);
+      return {
+        lastCheckpoint,
+      } as CurrentMessageProcessorPosition<CheckpointType>;
+    },
+    get isActive() {
+      return isActive;
+    },
+    handle: async (
+      messages: RecordedMessage<MessageType, MessageMetadataType>[],
+      partialContext: Partial<HandlerContext>,
+    ): Promise<MessageHandlerResult> => {
+      if (!isActive) return;
 
-//       return pool.withTransaction(async (transaction) => {
-//         let result: MessageProcessorMessageHandlerResult | undefined =
-//           undefined;
+      const scope = options.processingScope!(partialContext);
 
-//         let lastProcessedPosition: bigint | null = null;
+      return scope(async (context) => {
+        let result: MessageHandlerResult = undefined;
 
-//         for (const message of messages) {
-//           const typedMessage = message as ReadEvent<
-//             EventType,
-//             ReadEventMetadataWithGlobalPosition
-//           >;
+        let lastCheckpoint: CheckpointType | null = null;
 
-//           const client =
-//             (await transaction.connection.open()) as NodePostgresClient;
+        for (const message of messages) {
+          const messageProcessingResult = await eachMessage(message, context);
 
-//           const messageProcessingResult = await eachMessage(typedMessage, {
-//             execute: transaction.execute,
-//             connection: {
-//               connectionString,
-//               pool,
-//               transaction: transaction,
-//               client,
-//             },
-//           });
+          if (checkpoints) {
+            const storeCheckpointResult: StoreProcessorCheckpointResult<CheckpointType | null> =
+              await checkpoints.store(
+                {
+                  processorId: options.processorId,
+                  version: options.version,
+                  message,
+                  lastCheckpoint,
+                  partition: options.partition,
+                },
+                context,
+              );
 
-//           // TODO: Add correct handling of the storing checkpoint
-//           await storeProcessorCheckpoint(transaction.execute, {
-//             processorId: options.processorId,
-//             version: options.version,
-//             lastProcessedPosition,
-//             newPosition: typedMessage.metadata.globalPosition,
-//             partition: options.partition,
-//           });
+            if (storeCheckpointResult && storeCheckpointResult.success) {
+              // TODO: Add correct handling of the storing checkpoint
+              lastCheckpoint = storeCheckpointResult.newCheckpoint;
+            }
+          }
 
-//           lastProcessedPosition = typedMessage.metadata.globalPosition;
+          if (
+            messageProcessingResult &&
+            messageProcessingResult.type === 'STOP'
+          ) {
+            isActive = false;
+            result = messageProcessingResult;
+            break;
+          }
 
-//           if (
-//             messageProcessingResult &&
-//             messageProcessingResult.type === 'STOP'
-//           ) {
-//             isActive = false;
-//             result = messageProcessingResult;
-//             break;
-//           }
+          if (options.stopAfter && options.stopAfter(message)) {
+            isActive = false;
+            result = { type: 'STOP', reason: 'Stop condition reached' };
+            break;
+          }
 
-//           if (options.stopAfter && options.stopAfter(typedMessage)) {
-//             isActive = false;
-//             result = { type: 'STOP', reason: 'Stop condition reached' };
-//             break;
-//           }
+          if (
+            messageProcessingResult &&
+            messageProcessingResult.type === 'SKIP'
+          )
+            continue;
+        }
 
-//           if (
-//             messageProcessingResult &&
-//             messageProcessingResult.type === 'SKIP'
-//           )
-//             continue;
-//         }
+        return result;
+      });
+    },
+  };
+};
 
-//         return result;
-//       });
-//     },
-//   };
-// };
+export const projectionProcessor = <
+  EventType extends Event = Event,
+  EventMetaDataType extends
+    AnyRecordedMessageMetadata = AnyRecordedMessageMetadata,
+  HandlerContext extends DefaultRecord = DefaultRecord,
+  CheckpointType = GlobalPositionTypeOfRecordedMessageMetadata<EventMetaDataType>,
+>(
+  options: ProjectionProcessorOptions<EventType>,
+): MessageProcessor<
+  EventType,
+  EventMetaDataType,
+  HandlerContext,
+  CheckpointType
+> => {
+  const projection = options.projection;
 
-// export const projectionProcessor = <EventType extends Event = Event>(
-//   options: ProjectionProcessorOptions<EventType>,
-// ): MessageProcessor => {
-//   const projection = options.projection;
+  return genericMessageProcessor<
+    EventType,
+    EventMetaDataType,
+    HandlerContext,
+    CheckpointType
+  >({
+    processorId: options.processorId ?? `projection:${projection.name}`,
+    eachMessage: async (
+      event: RecordedMessage<EventType, EventMetaDataType>,
+      context: HandlerContext,
+    ) => {
+      if (!projection.canHandle.includes(event.type)) return;
 
-//   return genericMessageProcessor<EventType>({
-//     processorId: options.processorId ?? `projection:${projection.name}`,
-//     eachMessage: async (event, context) => {
-//       if (!projection.canHandle.includes(event.type)) return;
+      await projection.handle([event], context);
+    },
+    ...options,
+  });
+};
 
-//       await projection.handle([event], context);
-//     },
-//     ...options,
-//   });
-// };
+export const messageProcessor = <
+  MessageType extends Message = AnyMessage,
+  MessageMetadataType extends AnyReadEventMetadata = AnyReadEventMetadata,
+  HandlerContext extends DefaultRecord = DefaultRecord,
+  CheckpointType = GlobalPositionTypeOfRecordedMessageMetadata<MessageMetadataType>,
+>(
+  options: MessageProcessorOptions<
+    MessageType,
+    MessageMetadataType,
+    HandlerContext,
+    CheckpointType
+  >,
+): MessageProcessor<
+  MessageType,
+  MessageMetadataType,
+  HandlerContext,
+  CheckpointType
+> => {
+  if ('projection' in options) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+    return projectionProcessor(options as any) as MessageProcessor<
+      MessageType,
+      MessageMetadataType,
+      HandlerContext,
+      CheckpointType
+    >;
+  }
 
-// export const messageProcessor = <EventType extends Event = Event>(
-//   options: MessageProcessorOptions<EventType>,
-// ): MessageProcessor => {
-//   if ('projection' in options) {
-//     return projectionProcessor(options);
-//   }
-
-//   return genericMessageProcessor(options);
-// };
+  return genericMessageProcessor(options);
+};
