978d72dad8d88bc81a479ad174be81e7e1fdeb64
oskardudycz <oskar.dudycz@gmail.com>
2025-11-23 20:20:40 +0100

    Added SQLite samples as a copy of PostgrSQL

---
+++ samples/webApi/expressjs-with-sqlite/src/shoppingCarts/api.int.spec.ts
@@ -0,0 +1,156 @@
+import {
+  getInMemoryEventStore,
+  getInMemoryMessageBus,
+  type EventStore,
+} from '@event-driven-io/emmett';
+import {
+  ApiSpecification,
+  existingStream,
+  expectError,
+  expectNewEvents,
+  expectResponse,
+  getApplication,
+} from '@event-driven-io/emmett-expressjs';
+import { randomUUID } from 'node:crypto';
+import { beforeEach, describe, it } from 'node:test';
+import { shoppingCartApi } from './api';
+import { type PricedProductItem, type ShoppingCartEvent } from './shoppingCart';
+
+const getUnitPrice = (_productId: string) => {
+  return Promise.resolve(100);
+};
+
+void describe('ShoppingCart', () => {
+  let clientId: string;
+  let shoppingCartId: string;
+  beforeEach(() => {
+    clientId = randomUUID();
+    shoppingCartId = `shopping_cart:${clientId}:current`;
+  });
+
+  void describe('When empty', () => {
+    void it('should add product item', () => {
+      return given()
+        .when((request) =>
+          request
+            .post(`/clients/${clientId}/shopping-carts/current/product-items`)
+            .send(productItem),
+        )
+        .then([
+          expectNewEvents(shoppingCartId, [
+            {
+              type: 'ProductItemAddedToShoppingCart',
+              data: {
+                shoppingCartId,
+                clientId,
+                productItem,
+                addedAt: now,
+              },
+              metadata: { clientId },
+            },
+          ]),
+        ]);
+    });
+  });
+
+  void describe('When opened with product item', () => {
+    void it('should confirm', () => {
+      return given(
+        existingStream(shoppingCartId, [
+          {
+            type: 'ProductItemAddedToShoppingCart',
+            data: {
+              shoppingCartId,
+              clientId,
+              productItem,
+              addedAt: oldTime,
+            },
+            metadata: { clientId },
+          },
+        ]),
+      )
+        .when((request) =>
+          request.post(`/clients/${clientId}/shopping-carts/current/confirm`),
+        )
+        .then([
+          expectResponse(204),
+          expectNewEvents(shoppingCartId, [
+            {
+              type: 'ShoppingCartConfirmed',
+              data: {
+                shoppingCartId,
+                confirmedAt: now,
+              },
+              metadata: { clientId },
+            },
+          ]),
+        ]);
+    });
+  });
+
+  void describe('When confirmed', () => {
+    void it('should not add products', () => {
+      return given(
+        existingStream(shoppingCartId, [
+          {
+            type: 'ProductItemAddedToShoppingCart',
+            data: {
+              shoppingCartId,
+              clientId,
+              productItem,
+              addedAt: oldTime,
+            },
+            metadata: { clientId },
+          },
+          {
+            type: 'ShoppingCartConfirmed',
+            data: { shoppingCartId, confirmedAt: oldTime },
+            metadata: { clientId },
+          },
+        ]),
+      )
+        .when((request) =>
+          request
+            .post(`/clients/${clientId}/shopping-carts/current/product-items`)
+            .send(productItem),
+        )
+        .then(
+          expectError(403, {
+            detail: 'Shopping Cart already closed',
+            status: 403,
+            title: 'Forbidden',
+            type: 'about:blank',
+          }),
+        );
+    });
+  });
+
+  const oldTime = new Date();
+  const now = new Date();
+
+  const given = ApiSpecification.for<ShoppingCartEvent>(
+    (): EventStore => getInMemoryEventStore(),
+    (eventStore: EventStore) =>
+      getApplication({
+        apis: [
+          shoppingCartApi(
+            eventStore,
+            undefined!, //TODO: define recommendation how to use gets here
+            getInMemoryMessageBus(),
+            getUnitPrice,
+            () => now,
+          ),
+        ],
+      }),
+  );
+
+  const getRandomProduct = (): PricedProductItem => {
+    return {
+      productId: randomUUID(),
+      unitPrice: 100,
+      quantity: Math.random() * 10,
+    };
+  };
+
+  const productItem = getRandomProduct();
+});
