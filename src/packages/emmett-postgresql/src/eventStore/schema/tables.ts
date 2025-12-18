bb68d8c659c853f38e43ddd0ee3b0720c7eb2138
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-12-18 14:12:59 +0100

    Applied "Column tetris" from https://www.enterprisedb.com/blog/rocks-and-sand

--- src/packages/emmett-postgresql/src/eventStore/schema/tables.ts
+++ src/packages/emmett-postgresql/src/eventStore/schema/tables.ts
@@ -28,19 +28,19 @@ export const messagesTableSQL = rawSql(
   CREATE SEQUENCE IF NOT EXISTS emt_global_message_position;
 
   CREATE TABLE IF NOT EXISTS ${messagesTable.name}(
-      stream_id              TEXT                      NOT NULL,
       stream_position        BIGINT                    NOT NULL,
-      partition              TEXT                      NOT NULL DEFAULT '${globalTag}',
-      message_kind           CHAR(1)                   NOT NULL DEFAULT 'E',
-      message_data           JSONB                     NOT NULL,
-      message_metadata       JSONB                     NOT NULL,
-      message_schema_version TEXT                      NOT NULL,
-      message_type           TEXT                      NOT NULL,
-      message_id             TEXT                      NOT NULL,
-      is_archived            BOOLEAN                   NOT NULL DEFAULT FALSE,
       global_position        BIGINT                    DEFAULT nextval('emt_global_message_position'),
       transaction_id         XID8                      NOT NULL,
       created                TIMESTAMPTZ               NOT NULL DEFAULT now(),
+      is_archived            BOOLEAN                   NOT NULL DEFAULT FALSE,
+      message_kind           VARCHAR(1)                NOT NULL DEFAULT 'E',
+      stream_id              TEXT                      NOT NULL,
+      partition              TEXT                      NOT NULL DEFAULT '${globalTag}',
+      message_schema_version TEXT                      NOT NULL,
+      message_id             TEXT                      NOT NULL,
+      message_type           TEXT                      NOT NULL,
+      message_data           JSONB                     NOT NULL,
+      message_metadata       JSONB                     NOT NULL,
       PRIMARY KEY (stream_id, stream_position, partition, is_archived)
   ) PARTITION BY LIST (partition);`,
 );
@@ -48,11 +48,11 @@ export const messagesTableSQL = rawSql(
 export const processorsTableSQL = rawSql(
   `
   CREATE TABLE IF NOT EXISTS ${processorsTable.name}(
-      processor_id                  TEXT                   NOT NULL,
-      version                       INT                    NOT NULL DEFAULT 1,
-      partition                     TEXT                   NOT NULL DEFAULT '${globalTag}',
-      last_processed_checkpoint     TEXT                   NOT NULL,
       last_processed_transaction_id XID8                   NOT NULL,
+      version                       INT                    NOT NULL DEFAULT 1,
+      processor_id                  TEXT                   NOT NULL,
+      partition                     TEXT                   NOT NULL DEFAULT '${globalTag}',
+      last_processed_checkpoint     TEXT                   NOT NULL,    
       processor_instance_id         TEXT                   DEFAULT gen_random_uuid(),
       PRIMARY KEY (processor_id, partition, version)
   ) PARTITION BY LIST (partition);
@@ -385,6 +385,9 @@ BEGIN
         ALTER TABLE IF EXISTS emt_subscriptions RENAME TO emt_processors;
         
         -- Rename columns
+        ALTER TABLE emt_messages
+            ALTER COLUMN message_kind TYPE VARCHAR(1);
+
         ALTER TABLE emt_processors 
             RENAME COLUMN subscription_id TO processor_id;
 
