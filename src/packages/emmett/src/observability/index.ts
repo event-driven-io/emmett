68e8d8fa4397a691a9c76291374cb124645eb5d9
Oskar Dudycz <oskar.dudycz@gmail.com>
2026-03-13 08:54:25 +0100

    Added the initial set of observability archetypes and basic unit tests for them

--- src/packages/emmett/src/observability/index.ts
+++ src/packages/emmett/src/observability/index.ts
@@ -1,5 +1,5 @@
 export * from '@event-driven-io/almanac';
 export * from './attributes';
-export * from './collectors';
 export * from './options';
 export * from './tracer';
+export * from './collectors';
