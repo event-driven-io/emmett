74777df95f6c8f6443328fed0d5e6de8b675d718
Oskar Dudycz <oskar.dudycz@gmail.com>
2026-03-31 09:22:39 +0200

    Added missing snippets for projections docs

---
+++ src/docs/guides/projections/testingProjections.snippet.ts
@@ -0,0 +1,155 @@
+import type { Event } from '@event-driven-io/emmett';
+import {
+  eventInStream,
+  eventsInStream,
+  expectPongoDocuments,
+  newEventsInStream,
+  pongoSingleStreamProjection,
+  PostgreSQLProjectionSpec,
+} from '@event-driven-io/emmett-postgresql';
+import { getPostgreSQLStartedContainer } from '@event-driven-io/emmett-testcontainers';
+import type { StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+import { v4 as uuid } from 'uuid';
+import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest';
+
+type ProductItemAdded = Event<
+  'ProductItemAdded',
+  {
+    productItem: { price: number; productId: string; quantity: number };
+  }
+>;
+
+type DiscountApplied = Event<
+  'DiscountApplied',
+  {
+    percent: number;
+    couponId: string;
+  }
+>;
+
+type ShoppingCartShortInfo = {
+  productItemsCount: number;
+  totalAmount: number;
+  appliedDiscounts: string[];
+};
+
+const shoppingCartShortInfoCollectionName = 'shoppingCartShortInfo';
+
+const evolve = (
+  document: ShoppingCartShortInfo,
+  { type, data: event }: ProductItemAdded | DiscountApplied,
+): ShoppingCartShortInfo => {
+  switch (type) {
+    case 'ProductItemAdded':
+      return {
+        ...document,
+        totalAmount:
+          document.totalAmount +
+          event.productItem.price * event.productItem.quantity,
+        productItemsCount:
+          document.productItemsCount + event.productItem.quantity,
+      };
+    case 'DiscountApplied':
+      if (document.appliedDiscounts.includes(event.couponId)) return document;
+
+      return {
+        ...document,
+        totalAmount: (document.totalAmount * (100 - event.percent)) / 100,
+        appliedDiscounts: [...document.appliedDiscounts, event.couponId],
+      };
+    default:
+      return document;
+  }
+};
+
+const shoppingCartShortInfoProjection = pongoSingleStreamProjection({
+  collectionName: shoppingCartShortInfoCollectionName,
+  evolve,
+  canHandle: ['ProductItemAdded', 'DiscountApplied'],
+  initialState: () => ({
+    productItemsCount: 0,
+    totalAmount: 0,
+    appliedDiscounts: [],
+  }),
+});
+
+// #region testing-projection
+void describe('Shopping Cart Short Info Projection', () => {
+  let postgres: StartedPostgreSqlContainer;
+  let given: PostgreSQLProjectionSpec<ProductItemAdded | DiscountApplied>;
+  let shoppingCartId: string;
+
+  beforeAll(async () => {
+    postgres = await getPostgreSQLStartedContainer();
+
+    given = PostgreSQLProjectionSpec.for({
+      projection: shoppingCartShortInfoProjection,
+      connectionString: postgres.getConnectionUri(),
+    });
+  });
+
+  beforeEach(() => (shoppingCartId = `shoppingCart:${uuid()}:${uuid()}`));
+
+  afterAll(async () => {
+    await postgres.stop();
+  });
+
+  void it('creates summary from first event', () =>
+    given([])
+      .when([
+        eventInStream(shoppingCartId, {
+          type: 'ProductItemAdded',
+          data: {
+            productItem: { price: 100, productId: 'shoes', quantity: 100 },
+          },
+        }),
+      ])
+      .then(
+        expectPongoDocuments
+          .fromCollection<ShoppingCartShortInfo>(
+            shoppingCartShortInfoCollectionName,
+          )
+          .withId(shoppingCartId)
+          .toBeEqual({
+            productItemsCount: 100,
+            totalAmount: 10000,
+            appliedDiscounts: [],
+          }),
+      ));
+
+  void it('accumulates across events', () => {
+    const couponId = uuid();
+
+    return given(
+      eventsInStream<ProductItemAdded>(shoppingCartId, [
+        {
+          type: 'ProductItemAdded',
+          data: {
+            productItem: { price: 100, productId: 'shoes', quantity: 100 },
+          },
+        },
+      ]),
+    )
+      .when(
+        newEventsInStream(shoppingCartId, [
+          {
+            type: 'DiscountApplied',
+            data: { percent: 10, couponId },
+          },
+        ]),
+      )
+      .then(
+        expectPongoDocuments
+          .fromCollection<ShoppingCartShortInfo>(
+            shoppingCartShortInfoCollectionName,
+          )
+          .withId(shoppingCartId)
+          .toBeEqual({
+            productItemsCount: 100,
+            totalAmount: 9000,
+            appliedDiscounts: [couponId],
+          }),
+      );
+  });
+});
+// #endregion testing-projection
