35fbc140d93afe32bb3353cbdeb839fd7968068b
Oskar Dudycz <oskar.dudycz@gmail.com>
2026-04-22 10:16:41 +0200

    Upgraded eslint in samples

---
+++ samples/webApi/expressjs-with-postgresql/eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/', 'docker/', '**/*.d.ts', 'src/types/'],
+  },
+  js.configs.recommended,
+  ...typescriptEslint.configs['flat/recommended'],
+  ...typescriptEslint.configs['flat/recommended-type-checked'],
+  prettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+
+    rules: {
+      'no-unused-vars': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      '@typescript-eslint/no-misused-promises': ['off'],
+      '@typescript-eslint/prefer-namespace-keyword': 'off',
+    },
+  },
+];
